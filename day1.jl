d8610f392371186df82c51bb72409a74f7436d08
Justin Gray <justin.s.gray@gmail.com>
2022-12-01 07:19:04 -0500

    day1 done

--- day1.jl
+++ day1.jl
@@ -1,17 +1,21 @@
 open("inputs/day1.txt") do f
 
-    sum = 0
+    e_total = 0
     elfs = Int64[]
     while !eof(f)
         l = readline(f)
         if !isempty(l)
-            sum += parse(Int64,l)
+            e_total += parse(Int64,l)
         else
-            push!(elfs, sum)
-            sum = 0
+            push!(elfs, e_total)
+            e_total = 0
         end
     end
 
+    # part 1
     mxval, mxidx = findmax(elfs)
     @show mxval, mxidx
+
+    p = sortperm(elfs; rev=true)
+    @show sum(elfs[p[1:3]])
 end
